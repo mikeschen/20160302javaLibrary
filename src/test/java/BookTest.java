7871c384d7bdd83486d8b603e448db8d6b8ecc06
mike gallegos and michael schennum <mike@>
2016-03-02 08:21:14 -0800

    initial commit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+
+//Update Class name here and file name
+public class BookTest {
+
+	@Rule
+	public DatabaseRule database = new DatabaseRule();
+
+	// @Test
+	// public void all_emptyAtFirst() {
+	// 	assertEquals(Cuisine.all().size(), 0);
+	// }
+}
