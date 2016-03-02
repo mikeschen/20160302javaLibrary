c5d9cc7a7585a05c1d091faa3d4ecb35c631bdc4
michael schennum and mike gallegos <michael+mike@>
2016-03-02 14:41:01 -0800

    update book.vtl

---
+++ src/main/java/Copy.java
@@ -0,0 +1,16 @@
+public class Copy {
+  private int id;
+  private int count;
+
+  public Copy(int count){
+    this.count = count;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getCount() {
+    return count;
+  }
+}
